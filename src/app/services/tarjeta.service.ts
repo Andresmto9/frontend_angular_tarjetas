8e4592704917fea6972f6cb110d927de079969bc
Lisandro <lisandro.zubieta@infotegra.com>
2024-10-22 00:56:48 -0500

    Creación y mostrado en tabla de tarjetas creadas

--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,9 +1,108 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TarjetaService {
-
   constructor() { }
+
+  private tarjetasUpdated = new Subject<void>();
+
+  getTarjetasUpdatedListener() {
+    return this.tarjetasUpdated.asObservable();
+  }
+
+  notifyTarjetasUpdated() {
+    this.tarjetasUpdated.next();
+  }
+
+  getData(url: string): Promise<any> {
+    return fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error);
+        throw error;
+      });
+  }
+
+  getDataById(url: string): Promise<any>{
+    return fetch(url, {
+      method: 'GET',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => {
+        if (!response.ok){
+          throw new Error('Error por parte del servidor');
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("Error del fecth para actualziar")
+        throw error;
+      })
+  }
+
+  postData(url: string, data: any): Promise<any> {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Error por parte del servidor');
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.error('Error generado por el fetch:', error);
+        throw error;
+      });
+  }
+
+  updateData(url: string, data: any): Promise<any>{
+    return fetch(url, {
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("Error del fecth para actualziar")
+        throw error;
+      })
+  }
+
+  deleteData(url: string): Promise<any>{
+    return fetch(url, {
+      method: 'DELETE',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => {
+        if (!response.ok){
+          throw new Error('Error por parte del servidor');
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        console.log("Error del fecth para actualziar")
+        throw error;
+      })
+  }
 }
