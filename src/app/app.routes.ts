dbce1f3d6d95c53ed1a191fc5f7612a4b06ba024
lazubietac <lisandro.zubieta@infotegra.com>
2024-10-22 14:04:37 -0500

    División de componentes por rutas y creación login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'tarjeta', component: TarjetasComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { TarjetaCreditoComponent } from "./tarjeta-credito/tarjeta-credito.component"
 import { ListTarjetaCreditoComponent } from "./list-tarjeta-credito/list-tarjeta-credito.component"
+import { FooterComponent } from "../footer/footer.component"
 
 @Component({
   selector: 'app-tarjetas',
   standalone: true,
   imports: [
     TarjetaCreditoComponent,
-    ListTarjetaCreditoComponent
+    ListTarjetaCreditoComponent,
+    FooterComponent
   ],
   templateUrl: './tarjetas.component.html',
   styleUrl: './tarjetas.component.css'
